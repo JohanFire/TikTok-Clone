b48bc262770eae9c18cd74391e442a20184780b5
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-05-15 22:16:07 -0600

    Testing buttons & icons from React-native-elements

--- TikTok/App.js
+++ TikTok/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 
 export default function App() {
   return (
@@ -8,6 +9,41 @@ export default function App() {
         TikTok - Clone
       </Text>
       <StatusBar style="auto" />
+      <Button
+        title="Solid Button"
+      />
+
+      <Button
+        title="Outline button"
+        type="outline"
+      />
+
+      <Button
+        title="Clear button"
+        type="clear"
+      />
+      <Icon
+        name='rowing' />
+      <Icon
+        name='g-translate'
+        color='#00aced' />
+      <Icon
+        name='sc-telegram'
+        type='evilicon'
+        color='#517fa4'
+      />
+      <Icon
+        reverse
+        name='ios-american-football'
+        type='ionicon'
+        color='#517fa4'
+      />
+      <Icon
+        raised
+        name='heartbeat'
+        type='font-awesome'
+        color='#f50'
+        onPress={() => console.log('hello')} />
     </View>
   );
 }
