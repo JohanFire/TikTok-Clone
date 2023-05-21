4b6e44a982e1cd73c96e0f8b65c51d5fd66b7463
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-05-21 17:39:19 -0600

    Creating Register user form

--- TikTok/src/screens/Auth/RegisterEmailScreen/RegisterEmailScreen.js
+++ TikTok/src/screens/Auth/RegisterEmailScreen/RegisterEmailScreen.js
@@ -1,11 +1,57 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Text } from "react-native-elements";
+import React, { useState } from 'react'
+import { View, SafeAreaView } from 'react-native'
+import { Input, Button } from "react-native-elements";
 
-export function RegisterEmailScreen() {
+import { styles } from "./RegisterEmailScreen.styles";
+
+export function RegisterEmailScreen(props) {
+    const { navigation } = props;
+    const [showPassword, setShowPassword] = useState(false);
+
+    const onShowPassword = () => setShowPassword(prevState => !prevState)
+
     return (
-        <View>
-            <Text>RegisterEmailScreen</Text>
-        </View>
+        <SafeAreaView style={styles.content}>
+            <View style={styles.form}>
+                <Input
+                    placeholder='Correo electrónico'
+                    autoCapitalize='none'
+                />
+                <Input
+                    placeholder='Nombre(s)'
+                    autoCapitalize='none'
+                />
+                <Input
+                    placeholder='Apellido(s)'
+                    autoCapitalize='none'
+                />
+                <Input
+                    placeholder='Usuario'
+                    autoCapitalize='none'
+                />
+                <Input
+                    placeholder='Contraseña'
+                    secureTextEntry={!showPassword}
+                    rightIcon={{
+                        type: "material-community",
+                        name: showPassword ? "eye-off-outline" : "eye-outline",
+                        onPress: onShowPassword
+                    }}
+                    />
+                <Input
+                    placeholder='Confirmar Contraseña'
+                    secureTextEntry={!showPassword}
+                    rightIcon={{
+                        type: "material-community",
+                        name: showPassword ? "eye-off-outline" : "eye-outline",
+                        onPress: onShowPassword
+                    }}
+                />
+            </View>
+            <Button
+                title="Registrarse"
+                containerStyle={styles.btnContainer}
+            />
+        </SafeAreaView>
     )
 }
