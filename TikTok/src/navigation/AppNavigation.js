09d71ab4e2c2b4e6d1bf784f136f719536e72f38
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-06 00:08:40 -0600

    leaving a logout button in case wanna logout

--- TikTok/src/navigation/AppNavigation.js
+++ TikTok/src/navigation/AppNavigation.js
@@ -3,8 +3,9 @@
 import React from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button } from "react-native-elements";
 
-import { useTheme } from "../hooks";
+import { useTheme, useAuth } from "../hooks";
 import { getNavigationTheme, screen } from "../utils";
 import { TestScreen } from "../screens/TestScreen";
 import { TabNavigation } from "./TabNavigation";
@@ -14,12 +15,18 @@ const Stack = createNativeStackNavigator();
 export function AppNavigation() {
     const { theme } = useTheme();
     const MyTheme = getNavigationTheme(theme);
+    const { logout } = useAuth();
 
     return (
         <NavigationContainer theme={MyTheme}>
             <Stack.Navigator>
                 <Stack.Screen name={screen.app.tab} component={TabNavigation} options={{ headerShown: false }} />
             </Stack.Navigator>
+            {/* <Button
+                title="Cerrar sesión"
+                onPress={logout}
+            /> */}
         </NavigationContainer>
+        
     );
 }
