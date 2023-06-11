23421a46facabcd4661e3e3c246f63cef7b6e562
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-10 20:35:34 -0600

    Sort query's response

--- TikTok-server/TikTok/video/api/views.py
+++ TikTok-server/TikTok/video/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import OrderingFilter
 
 from video.models import Video
 from video.api.serializers import VideoSerializer
@@ -11,3 +12,5 @@ class VideoApiViewSet(ModelViewSet):
     serializer_class = VideoSerializer
     queryset = Video.objects.all()
     http_method_names = ['get', 'post']
+    filter_backends = [OrderingFilter]
+    ordering = ['-created_at'] # ['-'] = mayor a menor = newer to older
