1a7ac3b43ef1eb0a832d985b2c0473e6b70ce1b4
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-11 13:14:52 -0600

    Swipe animation to navigate through videos

--- TikTok/src/components/Home/ForYouVideos.js
+++ TikTok/src/components/Home/ForYouVideos.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView } from 'react-native'
+import { FlatList, Dimensions, View } from 'react-native'
 import { Text } from 'react-native-elements'
 
 import { Video } from "../../api";
 import { useAuth } from "../../hooks";
+import { ENV } from "../../utils";
 
 const videoController = new Video();
+const { height } = Dimensions.get("window")
 
 export function ForYouVideos() {
     const [videos, setVideos] = useState(null);
@@ -24,9 +26,33 @@ export function ForYouVideos() {
         }
     }, [])
 
+    if (!videos) return null;
+
     return (
-        <SafeAreaView>
-            <Text>ForYouVideos</Text>
-        </SafeAreaView>
+        <FlatList
+            data={videos}
+            decelerationRate="fast"
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({ item, index }) => (
+                <View index={index} style={{ height: height - ENV.TAB_MENU_HEIGHT }} >
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.description}</Text>
+                </View>
+            )}
+            removeClippedSubviews={false}
+            showsVerticalScrollIndicator={false}
+            snapToInterval={height - ENV.TAB_MENU_HEIGHT}
+            viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50}}
+            onScrollToIndexFailed={() => {}}
+        />
     )
 }
--- TikTok/src/utils/constants.js
+++ TikTok/src/utils/constants.js
@@ -3,6 +3,7 @@ const SERVER_IP = "192.168.1.13:8000";
 export const ENV = {
     BASE_PATH: `http://${SERVER_IP}`,
     BASE_API: `http://${SERVER_IP}/api`,
+
     API_ROUTES: {
         REGISTER: "auth/register",
         LOGIN: "auth/login",
@@ -18,5 +19,7 @@ export const ENV = {
     TYPE_VIDEO: {
         FOLLOWING: "following",
         FOR_YOU: "forYou",
-    }
+    },
+
+    TAB_MENU_HEIGHT: 70,
 }
