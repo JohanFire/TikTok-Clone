4da4e1e3b7000d22a143d25e2213c3a2f2db919b
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-17 18:46:06 -0600

    Opening comments block

--- TikTok/src/components/Shared/VideoFeed/Comments/Comments.js
+++ TikTok/src/components/Shared/VideoFeed/Comments/Comments.js
@@ -1,33 +1,62 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, {useRef} from 'react'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import { Text, Icon } from 'react-native-elements'
+import RBSheet from "react-native-raw-bottom-sheet";
 
 import { nFormatter } from "../../../../utils";
+import { useTheme } from "../../../../hooks";
+
+const { height } = Dimensions.get("screen");
 
 export function Comments(props) {
     const { idUser, idVideo } = props;
+    const sheet = useRef();
+    const styles = styled();
 
-    const open_sheet = () => {
-        console.log("Open comment container");
-    }
+    const open_sheet = () => sheet.current.open();
+    const close_sheet = () => sheet.current.close();
 
     return (
-        <View style={styles.content} >
-            <Icon 
-                type='material-community'
-                name='comment-processing'
-                size={40}
-                onPress={open_sheet}
-            />
+        <>
+            <View style={styles.content} >
+                <Icon
+                    type='material-community'
+                    name='comment-processing'
+                    size={40}
+                    onPress={open_sheet}
+                />
 
-            <Text>{nFormatter(99)}</Text>
-        </View>
+                <Text>{nFormatter(99)}</Text>
+            </View>
+
+            <RBSheet 
+                ref={sheet} 
+                height={height - 200} 
+                openDuration={200}
+                keyboardAvoidingViewEnabled={false}
+                customStyles={{
+                    container: styles.rb_sheet_container,
+                }}
+            >
+                <Text>Caja de comentarios</Text>
+            </RBSheet>
+        </>
     )
 }
 
-const styles = StyleSheet.create({
-    content: {
-        alignItems: "center",
-        marginBottom: 20,
-    },
-})
+const styled = () => {
+    const {theme} = useTheme();
+    return(
+        StyleSheet.create({
+            content: {
+                alignItems: "center",
+                marginBottom: 20,
+            },
+            rb_sheet_container: {
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+                backgroundColor: theme.Default.background,
+            }
+        })
+    )
+}
