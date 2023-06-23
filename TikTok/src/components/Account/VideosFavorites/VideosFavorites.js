4be1117f74a82065616346ee08b173b08ba2daec
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-23 10:48:03 -0600

    Listing favorite user videos

--- TikTok/src/components/Account/VideosFavorites/VideosFavorites.js
+++ TikTok/src/components/Account/VideosFavorites/VideosFavorites.js
@@ -1,35 +1,95 @@
-import React, {useState, useEffect} from 'react'
-import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-elements'
+import React, { useState, useEffect, useCallback } from 'react'
+import { StyleSheet, View, Pressable, Dimensions } from 'react-native'
+import { Text, Image } from 'react-native-elements'
+import { map, size } from "lodash";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import { Video } from "../../../api";
 import { useAuth } from "../../../hooks";
+import { screen } from "../../../utils";
 
 const video = new Video();
+const { width } = Dimensions.get('screen')
 
 export function VideosFavorites(props) {
     const { idUser } = props;
-    const [videosFavorites, setVideosFavorites] = useState(null);
+    const [videos, setVideos] = useState(null);
     const { accessToken } = useAuth();
+    const navigation = useNavigation();
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await video.get_favorite_user_videos(accessToken, idUser)
-                console.log("videos_favorites: ", response);
-            } catch (error) {
-                console.error(error);
-            }
-        })()
-    }, [])
-    
+    const go_to_favorite_video = (data) => {
+        navigation.navigate(screen.app.videosFavorites, {
+            idUser,
+            idVideo: data.id,
+        });
+    };
+
+    useFocusEffect(
+        useCallback(
+            () => {
+                (async () => {
+                    try {
+                        const response = await video.get_favorite_user_videos(
+                            accessToken,
+                            idUser
+                        )
+                        console.log("response", response);
+                        setVideos(response)
+                    } catch (error) {
+                        console.error(error);
+                    }
+                })()
+            },
+            [],
+        )
+    )
+
+    if (!videos) return null;
+
     return (
-        <View>
-            <Text>VideosFavorites screen</Text>
+        <View style={styles.content}>
+            {map(videos, (video, index) => (
+                <Pressable
+                    key={index}
+                    style={styles.video__block}
+                    onPress={() => go_to_favorite_video(video)}
+                >
+                    <Image
+                        source={{ uri: video.video_data.image }}
+                        style={styles.image}
+                    />
+                </Pressable>
+            ))}
+
+            {size(videos) === 0 && (
+                <View style={styles.no__videos}>
+                    <Text>No tienes ningun video favorito</Text>
+                </View>
+            )}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    content: {
+        flex: 1,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        width: width,
+    },
+    video__block: {
+        width: "33.3333%",
+        height: 200,
+    },
+    no__videos: {
+        width: "100%",
+        alignItems: "center",
+        opacity: 0.6,
+        marginTop: 20,
+    },
+    image:{
+        width: "100%",
+        height: "100%",
+        resizeMode: "cover"
+    }
 })
