ae320e5c3148b3c847c46a781bcc6505474159c9
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-24 17:48:03 -0600

    Update Name

--- TikTok/src/screens/Account/ChangeNameScreen.js
+++ TikTok/src/screens/Account/ChangeNameScreen.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Text } from 'react-native-elements'
+import { Text, Input, Button } from 'react-native-elements'
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-export function ChangeNameScreen() {
+import { User } from "../../api";
+import { useAuth } from "../../hooks";
+
+const user = new User();
+
+export function ChangeNameScreen(props) {
+    const { route: { params }, navigation } = props;
+    const { accessToken } = useAuth();
+
+    const formik = useFormik({
+        initialValues: { name: params.name },
+        validationSchema: Yup.object({
+            name: Yup.string().required("El nombre es obligatorio"),
+        }),
+        validateOnChange: false,
+        onSubmit: async (formValue) => {
+            try {
+                await user.update_user(accessToken,
+                    { first_name: formValue.name }
+                )
+                navigation.goBack()
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })
+
     return (
-        <View>
-            <Text>ChangeNameScreen</Text>
+        <View style={{ marginHorizontal: 20 }}>
+            <Input
+                // value={""}
+                // onChange={() => { }}
+                placeholder='Nombre'
+                autoCapitalize='words'
+                value={formik.values.name}
+                onChangeText={text => formik.setFieldValue("name", text)}
+                errorMessage={formik.errors.name}
+            />
+            <Button
+                title="Guardar"
+                style={{ marginTop: 10 }}
+                onPress={formik.handleSubmit}
+                loading={formik.isSubmitting}
+            />
         </View>
     )
 }
