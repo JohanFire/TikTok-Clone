901ccabc63e1d57a00b7262cc5a9b2d3590891d6
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-27 16:50:55 -0600

    Incresing Access Token lifetime momentarily

--- TikTok-server/TikTok/TikTok/settings.py
+++ TikTok-server/TikTok/TikTok/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=7)
 }
 
