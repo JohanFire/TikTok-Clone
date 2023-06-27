2fba20b9155303a4219fc01a8bc265492fad75ec
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-26 23:15:40 -0600

    Adding search input

--- TikTok/src/screens/Friends/FriendsScreen.js
+++ TikTok/src/screens/Friends/FriendsScreen.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, SafeAreaView } from 'react-native'
 import { Text } from 'react-native-elements'
 
+import { Search } from "../../components/Friends";
+import { Users } from "../../api";
+import { useAuth } from "../../hooks";
+
+const userController = new Users();
+
 export function FriendsScreen() {
+    const { accessToken } = useAuth();
+    const [searchText, setSearchText] = useState("")
+    const [users, setUsers] = useState()
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await userController.obtains(
+                    accessToken,
+                    searchText
+                )
+                setUsers(response);
+            } catch (error) {
+                console.error(error);
+            }
+        })()
+    }, [searchText])
+
+
     return (
         <View>
-            <Text>FriendsScreen</Text>
+            <SafeAreaView>
+                <Search setSearchText={setSearchText} />
+            </SafeAreaView>
         </View>
     )
 }
