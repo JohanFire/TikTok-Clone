854ab9dc1556fb32a91b0b277fff1d5a27de6077
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-26 21:34:34 -0600

    Rendering published user videos

--- TikTok/src/screens/Shared/VideosPublishedScreen.js
+++ TikTok/src/screens/Shared/VideosPublishedScreen.js
@@ -1,36 +1,70 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { View, FlatList, Dimensions } from 'react-native'
 import { Text } from 'react-native-elements'
+import { forEach } from "lodash";
 
 import { useAuth } from "../../hooks";
 import { Video } from "../../api";
+import { VideoFeed } from "../../components/Shared";
 
 const video = new Video();
+const { height } = Dimensions.get("window");
 
 export function VideosPublishedScreen(props) {
-    const { route: { params } } = props;
-    const { accessToken } = useAuth();
+    const {
+        route: { params },
+    } = props;
     const [videos, setVideos] = useState(null);
+    const [indexStart, setIndexStart] = useState(null);
+    const { accessToken } = useAuth();
 
     useEffect(() => {
         (async () => {
             try {
-                const response = await video.get_user_videos(
-                    accessToken,
-                    params.idUser
-                );
-                setVideos(response)
-                console.log(response);
+                const response = await video.get_user_videos(accessToken, params.idUser);
+                setVideos(response);
             } catch (error) {
                 console.error(error);
             }
-        })()
-    }, [])
+        })();
+    }, [params]);
 
+    useEffect(() => {
+        if (videos) {
+            forEach(videos, (video, index) => {
+                if (video.id === params.idVideo) {
+                    setIndexStart(index);
+                }
+            });
+        }
+    }, [params, videos]);
+
+    const onViewChangeRef = useRef(({ viewableItems }) => {
+        setIndexStart(viewableItems[0].index);
+    });
+
+    if (!videos || indexStart === null) return null;
 
     return (
-        <View>
-            <Text>VideosPublishedScreen</Text>
-        </View>
-    )
+        <FlatList
+            data={videos}
+            decelerationRate="fast"
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({ item, index }) => (
+                <VideoFeed
+                    item={item}
+                    index={index}
+                    indexShow={indexStart}
+                    style={{ height }}
+                />
+            )}
+            removeClippedSubviews={false}
+            showsVerticalScrollIndicator={false}
+            snapToInterval={height}
+            onViewableItemsChanged={onViewChangeRef.current}
+            viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
+            initialScrollIndex={indexStart}
+            onScrollToIndexFailed={() => { }}
+        />
+    );
 }
