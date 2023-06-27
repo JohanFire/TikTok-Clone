f3ded749bfc1d345d7d1cd9e7958e9c0aa4201c6
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-26 21:10:13 -0600

    GETTING videos published by user

--- TikTok/src/screens/Shared/VideosPublishedScreen.js
+++ TikTok/src/screens/Shared/VideosPublishedScreen.js
@@ -1,8 +1,33 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, FlatList, Dimensions } from 'react-native'
 import { Text } from 'react-native-elements'
 
-export function VideosPublishedScreen() {
+import { useAuth } from "../../hooks";
+import { Video } from "../../api";
+
+const video = new Video();
+
+export function VideosPublishedScreen(props) {
+    const { route: { params } } = props;
+    const { accessToken } = useAuth();
+    const [videos, setVideos] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await video.get_user_videos(
+                    accessToken,
+                    params.idUser
+                );
+                setVideos(response)
+                console.log(response);
+            } catch (error) {
+                console.error(error);
+            }
+        })()
+    }, [])
+
+
     return (
         <View>
             <Text>VideosPublishedScreen</Text>
