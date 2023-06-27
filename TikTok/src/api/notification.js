905aecab9199a63c6ecfa44e65aed5779dbb54e6
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-27 17:52:43 -0600

    Creating LIKE Notification

---
+++ TikTok/src/api/notification.js
@@ -0,0 +1,39 @@
+import { ENV } from "../utils";
+
+export class Notification {
+    async create({
+        token,
+        idTargetUser,
+        idUserFollower,
+        idVideo,
+        comment,
+        typeNotification
+    }) {
+        try {
+            const url = `${ENV.BASE_API}/${ENV.API_ROUTES.NOTIFICATION}/`;
+            const params = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    user: idTargetUser,
+                    user_follower: idUserFollower,
+                    video: idVideo,
+                    comment: comment,
+                    type_notification: typeNotification,
+                }),
+            };
+
+            const response = await fetch(url, params)
+            const result = await response.json();
+
+            if(response.status !== 201) throw result;
+
+            return true;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
