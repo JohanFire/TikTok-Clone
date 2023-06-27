5bfda3fce88da354e3807d544ebf932623fc2c5d
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-26 21:41:56 -0600

    GETTING favorites videos of the user

--- TikTok/src/screens/Shared/VideosFavoritesScreen.js
+++ TikTok/src/screens/Shared/VideosFavoritesScreen.js
@@ -1,8 +1,34 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { View, FlatList, Dimensions } from 'react-native'
 import { Text } from 'react-native-elements'
+import { forEach } from "lodash";
 
-export function VideosFavoritesScreen() {
+import { useAuth } from "../../hooks";
+import { Video } from "../../api";
+import { VideoFeed } from "../../components/Shared";
+
+const video = new Video();
+
+export function VideosFavoritesScreen(props) {
+    const {
+        route: { params },
+    } = props;
+    const [videos, setVideos] = useState(null);
+    const [indexStart, setIndexStart] = useState(null);
+    const { accessToken } = useAuth();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await video.get_favorite_user_videos(accessToken, params.idUser);
+                setVideos(response);
+            } catch (error) {
+                console.error(error);
+            }
+        })()
+    }, [params])
+    
+
     return (
         <View>
             <Text>VideosFavoritesScreen</Text>
