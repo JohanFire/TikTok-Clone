087c6a07fe412ccd3806157e50745e1c41bac37e
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-27 18:19:56 -0600

    updating model

--- TikTok-server/TikTok/notification/models.py
+++ TikTok-server/TikTok/notification/models.py
@@ -4,32 +4,19 @@ NotificationTypeEnum = (
     ("FOLLOW", "follow"),
     ("COMMENT", "comment"),
     ("LIKE", "like"),
-    ("SHARED", "shared"),
+    ("SHARED", "shered"),
 )
 
+
 class Notification(models.Model):
-    # who receives the notification
     user = models.ForeignKey(
-        'users.User',
-        on_delete=models.CASCADE,
-        related_name="notification_user"
-    )
-    # who sends the notification
+        'users.User', on_delete=models.CASCADE, related_name="notification_user")
     user_follower = models.ForeignKey(
-        'users.User',
-        on_delete=models.CASCADE,
-        related_name="notification_user_follower"
-    )
+        'users.User', on_delete=models.CASCADE, related_name="notification_user_follower")
     video = models.ForeignKey(
-        'video.Video',
-        on_delete=models.CASCADE,
-        null=True, # not always will be video notification, maybe follow
-        blank=True
-    )
+        'video.Video', on_delete=models.CASCADE, null=True, blank=True)
     type_notification = models.CharField(
-        max_length=255,
-        choices=NotificationTypeEnum
-    )
+        max_length=255, choices=NotificationTypeEnum)
     comment = models.TextField(null=True, blank=True)
     read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
