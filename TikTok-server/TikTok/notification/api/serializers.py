79bf34625b7c1b5f6b0150f2ba552282fff7701b
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-06-27 21:40:10 -0600

    Bugfixed: serializers

--- TikTok-server/TikTok/notification/api/serializers.py
+++ TikTok-server/TikTok/notification/api/serializers.py
@@ -1,18 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-from users.api.serializers import UserSerializer
-
 from notification.models import Notification
-from video.api.serializers import VideoNotificationSerializer
+
+from users.api.serializers import UserSerializer
+from video.api.serializers import VideoSerializer
+
 
 class NotificationSerializer(ModelSerializer):
-    user_follower_data = UserSerializer(source="user_ follower", read_only=True)
-    video_data = VideoNotificationSerializer(source="video", read_only=True)
+    user_follower_data = UserSerializer(source="user_follower", read_only=True)
+    video_data = VideoSerializer(source="video", read_only=True)
 
     class Meta:
         model = Notification
-
-        fields = ['id', 'user', 'user_follower', 'user_follower_data',
-                    'video', 'video_data',
-                    'type_notification', 'comment',
-                    'read', 'created_at'
-                ]
+        fields = ['id', 'user', 'user_follower', 'user_follower_data', 'video', 'video_data',
+                'type_notification', 'comment', 'read', 'created_at']
